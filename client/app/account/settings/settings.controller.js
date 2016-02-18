eb1365e599ab82fab6bf915461bf804af91449e9
Edu <e.marin.izquierdo@gmail.com>
2016-02-18 21:35:35 +0100

    twitter profile updated

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -2,12 +2,10 @@
 
 angular.module('dropcubeApp')
   .controller('SettingsCtrl', function ($scope, $translate, User, Auth, Note) {
-    
+
     $scope.errors = {};
     $scope.note = Note;
     $scope.user = Auth.getCurrentUser(true).$promise.then(normalize);
-
-
 
     function __init__(){
 
@@ -16,16 +14,35 @@ angular.module('dropcubeApp')
     function normalize(user){
 
       user.normalized = {};
-      console.log(user);
+
+      user.normalized._id = user._id;
+      user.normalized.email = user.email;
+      user.normalized.lang = user.lang;
+
       if(user && user.provider == "google"){
 
-        user.normalized._id = user._id;
         user.normalized.name = user.google.displayName;
-        user.normalized.email = user.email;
-        user.normalized.lang = user.lang;
 
         if(user.google.image){
           user.normalized.photo = user.google.image.url.replace("?sz=50", "?sz=100");
+        }
+
+        if(user.google.cover && user.google.cover.coverPhoto && user.google.cover.coverPhoto.url){
+          user.normalized.cover = user.google.cover.coverPhoto.url;
+        }
+
+      }
+
+      if(user && user.provider == "twitter"){
+
+        user.normalized.name = user.twitter.name;
+
+        if(user.twitter.profile_image_url){
+          user.normalized.photo = user.twitter.profile_image_url;
+        }
+
+        if(user.twitter.profile_background_image_url_https){
+          user.normalized.cover = user.twitter.profile_background_image_url_https;
         }
 
       }
